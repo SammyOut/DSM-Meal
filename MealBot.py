a63da58afdfbfd832072b3b910a0fe547893cc1a
Sangmin <sm372181@gmail.com>
2017-05-17 22:01:32 +0900

    tomorrow 기능 추가
    tomorrow는 나이스에서 파싱

--- MealBot.py
+++ MealBot.py
@@ -10,12 +10,15 @@ class MealBot(fbchat.Client):
         self.markAsDelivered(author_id, mid) #mark delivered
         self.markAsRead(author_id) #mark read
 
-        if (message.find('아침')+1) :
-            self.send(author_id, parsing.breakfast())
-        elif (message.find('점심')+1) :
-            self.send(author_id, parsing.lunch())
-        elif (message.find('저녁')+1) :
-            self.send(author_id, parsing.dinner())
-        elif (message.find('밥')+1 or message.find('급식')+1) :
-            self.send(author_id,parsing.breakfast()+'\n'+parsing.lunch()+'\n'+parsing.dinner())
+        if str(author_id) != str(self.uid) :
+            if(message.find('내일')+1) :
+                self.send(author_id, parsing.tomorrow())
+            elif (message.find('아침')+1) :
+                self.send(author_id, parsing.breakfast())
+            elif (message.find('점심')+1) :
+                self.send(author_id, parsing.lunch())
+            elif (message.find('저녁')+1) :
+                self.send(author_id, parsing.dinner())
+            elif (message.find('밥')+1 or message.find('급식')+1) :
+                self.send(author_id,parsing.breakfast()+parsing.lunch()+parsing.dinner())
         message = ''
--- parsing.py
+++ parsing.py
@@ -1,17 +1,33 @@
 import urllib.request
+import time
 from bs4 import BeautifulSoup
 import re
 
-url = urllib.request.urlopen("http://dsm2015.cafe24.com")
-soup = BeautifulSoup(url, 'html.parser')
-menu = soup.find_all("p","menu")
+t = time.localtime()
+year = str(t.tm_year)
+month = str(t.tm_mon).zfill(2)
+day = t.tm_mday
+
+url1 = urllib.request.urlopen("http://stu.dje.go.kr/sts_sci_md00_001.do?schulCode=G100000170&schulCrseScCode=4&schulKndScCode=04&schYm="+year+month)
+url2 =urllib.request.urlopen("http://dsm2015.cafe24.com")
+
+soup1 = BeautifulSoup(url1, 'html.parser')
+soup2 = BeautifulSoup(url2, 'html.parser')
+
+tomo = (str(soup1.findAll('tbody')[0]).replace('<br/>', '\n').replace('</div></td>','\n').replace('<tbody>', '').replace('<tr>', '').replace('<td>', '').replace('<td class="last">', '').replace('</tr>', '')).split('<div>')
+today = soup2.find_all("p","menu")
+
+del tomo[0]
 
 def breakfast() :
-    breakfast = re.sub('[0-9]','',str(menu[0]).replace('<p class="col-9 menu">', '아침 : ').replace('</p>', '').replace('.', ''))
+    breakfast = re.sub('[0-9]','',str(today[0]).replace('<p class="col-9 menu">', '\n[조식]\n').replace('</p>', '').replace('.', '').replace(', ','\n'))+'\n'
     return breakfast
 def lunch() :
-    lunch = re.sub('[0-9]','',str(menu[1]).replace('<p class="col-9 menu">', '점심 : ').replace('</p>', '').replace('.', ''))
+    lunch = re.sub('[0-9]','',str(today[1]).replace('<p class="col-9 menu">', '\n[중식]\n').replace('</p>', '').replace('.', '').replace(', ','\n'))+'\n'
     return lunch
 def dinner() :
-    dinner = re.sub('[0-9]','',str(menu[2]).replace('<p class="col-9 menu">', '저녁 : ').replace('</p>', '').replace('.', ''))
+    dinner = re.sub('[0-9]','',str(today[2]).replace('<p class="col-9 menu">', '\n[석식]\n').replace('</p>', '').replace('.', '').replace(', ','\n'))+'\n'
     return dinner
+def tomorrow() :
+    tomorrow = re.sub('[0-9]','',tomo[day+1].replace('.', '').replace('*', '').replace('[중식]', '\n[중식]').replace('[석식]', '\n[석식]'))
+    return tomorrow
