d108e30e3b0b49ce24bddcaa7e5ac7741b9ac880
Sangmin <sm372181@gmail.com>
2017-05-18 21:13:59 +0900

    add many things
    이러저러한 기능 추가

--- MealBot.py
+++ MealBot.py
@@ -1,6 +1,7 @@
 import fbchat
 import parsing
 
+command = '모레 : 내일모레 하루 급식 메뉴 출력\n내일 : 내일 하루 급식 메뉴 출력\n아침 : 오늘 아침 급식 메뉴 출력\n점심 : 오늘 점심 급식 메뉴 출력\n저녁 : 오늘 저녁 급식 메뉴 출력\n급식, 밥 : 오늘 하루 급식 메뉴 출력'
 class MealBot(fbchat.Client):
 
     def __init__(self,email, password, debug=True, user_agent=None):
@@ -11,14 +12,30 @@ class MealBot(fbchat.Client):
         self.markAsRead(author_id) #mark read
 
         if str(author_id) != str(self.uid) :
-            if(message.find('내일')+1) :
-                self.send(author_id, parsing.tomorrow())
-            elif (message.find('아침')+1) :
+            if ('호서'in message) :
+                if('내일' in message) :
+                    self.send(author_id, parsing.htomo())
+                elif ('밥' in message or '급식' in message) :
+                    self.send(author_id,parsing.htoday())
+                    
+            elif('당진' in message) :
+                if('내일' in message) :
+                    self.send(author_id, parsing.dtomo())
+                elif ('밥' in message or '급식' in message) :
+                    self.send(author_id,parsing.dtoday())
+
+            elif ('모레'in message) :
+                    self.send(author_id, parsing.day2())    
+            elif('내일' in message) :
+                    self.send(author_id, parsing.tomorrow())
+            elif ('아침' in message) :
                 self.send(author_id, parsing.breakfast())
-            elif (message.find('점심')+1) :
+            elif ('점심' in message) :
                 self.send(author_id, parsing.lunch())
-            elif (message.find('저녁')+1) :
+            elif ('저녁' in message) :
                 self.send(author_id, parsing.dinner())
-            elif (message.find('밥')+1 or message.find('급식')+1) :
+            elif ('밥' in message or '급식' in message) :
                 self.send(author_id,parsing.breakfast()+parsing.lunch()+parsing.dinner())
+            elif ('명령어' in message) :
+                self.send(author_id,command)
         message = ''
--- parsing.py
+++ parsing.py
@@ -10,14 +10,23 @@ day = t.tm_mday
 
 url1 = urllib.request.urlopen("http://stu.dje.go.kr/sts_sci_md00_001.do?schulCode=G100000170&schulCrseScCode=4&schulKndScCode=04&schYm="+year+month)
 url2 =urllib.request.urlopen("http://dsm2015.cafe24.com")
+hurl = urllib.request.urlopen("http://stu.cne.go.kr/sts_sci_md00_001.do?schulCode=N100000269&schulCrseScCode=4&schulKndScCode=04&schYm="+year+month)
+durl = urllib.request.urlopen("http://stu.cne.go.kr/sts_sci_md00_001.do?schulCode=N100000154&schulCrseScCode=4&schulKndScCode=04&schYm="+year+month)
+
 
 soup1 = BeautifulSoup(url1, 'html.parser')
 soup2 = BeautifulSoup(url2, 'html.parser')
+hsoup = BeautifulSoup(hurl, 'html.parser')
+dsoup = BeautifulSoup(durl, 'html.parser')
 
 tomo = (str(soup1.findAll('tbody')[0]).replace('<br/>', '\n').replace('</div></td>','\n').replace('<tbody>', '').replace('<tr>', '').replace('<td>', '').replace('<td class="last">', '').replace('</tr>', '')).split('<div>')
 today = soup2.find_all("p","menu")
 
+h = (str(hsoup.findAll('tbody')[0]).replace('<br/>', '\n').replace('</div></td>','\n').replace('<tbody>', '').replace('<tr>', '').replace('<td>', '').replace('<td class="last">', '').replace('</tr>', '')).split('<div>')
+d = (str(dsoup.findAll('tbody')[0]).replace('<br/>', '\n').replace('</div></td>','\n').replace('<tbody>', '').replace('<tr>', '').replace('<td>', '').replace('<td class="last">', '').replace('</tr>', '')).split('<div>')
+
 del tomo[0]
+del h[0]
 
 def breakfast() :
     breakfast = re.sub('[0-9]','',str(today[0]).replace('<p class="col-9 menu">', '\n[조식]\n').replace('</p>', '').replace('.', '').replace(', ','\n'))+'\n'
@@ -31,3 +40,19 @@ def dinner() :
 def tomorrow() :
     tomorrow = re.sub('[0-9]','',tomo[day+1].replace('.', '').replace('*', '').replace('[중식]', '\n[중식]').replace('[석식]', '\n[석식]'))
     return tomorrow
+def day2() :
+    day2 = re.sub('[0-9]','',tomo[day+2].replace('.', '').replace('*', '').replace('[중식]', '\n[중식]').replace('[석식]', '\n[석식]'))
+    return day2 
+def htoday () :
+    htoday = re.sub('[0-9]','',h[day].replace('.', '').replace('*', '').replace('[중식]', '\n[중식]').replace('[석식]', '\n[석식]'))
+    return htoday
+def htomo () :
+    htomo = re.sub('[0-9]','',h[day+1].replace('.', '').replace('*', '').replace('[중식]', '\n[중식]').replace('[석식]', '\n[석식]'))
+    return htomo
+
+def dtoday() :
+    dtoday = re.sub('[0-9]','',d[day].replace('.', '').replace('*', '').replace('[중식]', '\n[중식]').replace('[석식]', '\n[석식]'))
+    return dtoday
+def dtomo () :
+    dtomo = re.sub('[0-9]','',d[day+1].replace('.', '').replace('*', '').replace('[중식]', '\n[중식]').replace('[석식]', '\n[석식]'))
+    return dtomo
