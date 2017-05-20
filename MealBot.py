1d175606bb96df2259196c81c9d33c2c877ba00f
Sangmin <sm372181@gmail.com>
2017-05-20 10:59:12 +0900

    code simplification
    소스 간소화

--- MealBot.py
+++ MealBot.py
@@ -1,5 +1,8 @@
 import fbchat
 import parsing
+import time
+
+t = time.localtime()
 
 class MealBot(fbchat.Client):
 
@@ -10,27 +13,20 @@ class MealBot(fbchat.Client):
         self.markAsDelivered(author_id, mid) #mark delivered
         self.markAsRead(author_id) #mark read
 
+        meal = 0
+        dayn = t.tm_mday
+
         if str(author_id) != str(self.uid) :
+            if (not('의견' in message or '아침' in message or '점심'in message or '저녁' in message or '모레' in message or '내일' in message or '밥' in messageor or '급식' in message)) : return 0
             if ('의견' in message) :
                 self.send(100004765026222,message)
                 self.send(author_id, "의견이 전송되었습니다.")
-              
-            elif('모레'in message) : self.send(author_id, parsing.tt())
-            
-            elif('내일' in message) :
-                if ('아침' in message) : self.send(author_id, parsing.tb())
-                elif ('점심' in message) : self.send(author_id, parsing.tl())
-                elif ('저녁' in message) : self.send(author_id, parsing.td())
-                else : self.send(author_id,parsing.tom())
-
-            elif ('오늘' in message ) :
-                if ('아침' in message) : self.send(author_id, parsing.br())
-                elif ('점심' in message) : self.send(author_id, parsing.lu())
-                elif ('저녁' in message) : self.send(author_id, parsing.di())
-                else : self.send(author_id,parsing.tod())
-                
-            elif ('아침' in message) : self.send(author_id, parsing.br())
-            elif ('점심' in message) : self.send(author_id, parsing.lu())
-            elif ('저녁' in message) : self.send(author_id, parsing.di())
+            else :
+                if ('아침' in message) : meal = 1
+                elif ('점심'in message) : meal = 2
+                elif ('저녁' in message) : meal = 3
+                if ('모레' in message) : dayn += 2
+                elif('내일' in message) : dayn += 1
+                self.send(author_id, parsing.bab(dayn, meal))
 
         message = ''
--- parsing.py
+++ parsing.py
@@ -6,7 +6,6 @@ import re
 t = time.localtime()
 year = str(t.tm_year)
 month = str(t.tm_mon).zfill(2)
-dayn = t.tm_mday
 
 url = urllib.request.urlopen("http://stu.dje.go.kr/sts_sci_md00_001.do?schulCode=G100000170&schulCrseScCode=4&schulKndScCode=04&schYm="+year+month)
 soup = BeautifulSoup(url, 'html.parser')
@@ -24,14 +23,6 @@ for a in daily :
     day.append(''.join(a).replace('[','SPL[').split('SPL'))
 
     
-def br() : return day[dayn][1]
-def lu() : return day[dayn][2]
-def di() : return day[dayn][3]
-def tod() : return day[dayn][1]+'\n'+day[dayn][2]+'\n'+day[dayn][3]
-
-def tb() : return day[dayn+1][1]
-def tl() : return day[dayn+1][2]
-def td() : return day[dayn+1][3]
-def tom() : return day[dayn+1][1]+'\n'+day[dayn+1][2]+'\n'+day[dayn+1][3]
-
-def tt() :return day[dayn+2][1]+'\n'+day[dayn+2][2]+'\n'+day[dayn+2][3]
+def bab(dayn, meal) :
+    if (meal == 0) : return day[dayn][1]+'\n'+day[dayn][2]+'\n'+day[dayn][3]
+    else : return day[dayn][meal]
