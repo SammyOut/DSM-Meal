36b3d9ca7b30172fefe853fd7a3e94b84a6c4fba
Sangmin <sm372181@gmail.com>
2017-05-20 14:51:26 +0900

    MealBot조건문 수정
    MealBot조건문 수정

--- MealBot.py
+++ MealBot.py
@@ -17,16 +17,21 @@ class MealBot(fbchat.Client):
         dayn = t.tm_mday
 
         if str(author_id) != str(self.uid) :
-            if (not('의견' in message or '아침' in message or '점심'in message or '저녁' in message or '모레' in message or '내일' in message or '밥' in messageor or '급식' in message)) : return 0
-            if ('의견' in message) :
-                self.send(100004765026222,message)
-                self.send(author_id, "의견이 전송되었습니다.")
-            else :
-                if ('아침' in message) : meal = 1
-                elif ('점심'in message) : meal = 2
-                elif ('저녁' in message) : meal = 3
-                if ('모레' in message) : dayn += 2
-                elif('내일' in message) : dayn += 1
-                self.send(author_id, parsing.bab(dayn, meal))
+            if ('의견' in message or '아침' in message or '점심'in message or '저녁' in message or '모레' in message or '내일' in message or '밥' in messageor or '급식' in message) : 
 
-        message = ''
+                if ('의견' in message) :
+                    f = open("opinion.txt", 'a')
+                    f.write(message+'\n')
+                    f.close()
+                    self.send(100004765026222, "의견이 추가됨")
+                    self.send(author_id, "의견이 전송되었습니다.")
+                    
+                else :
+                    if ('아침' in message) : meal = 1
+                    elif ('점심'in message) : meal = 2
+                    elif ('저녁' in message) : meal = 3
+                    if ('모레' in message) : dayn += 2
+                    elif('내일' in message) : dayn += 1
+                    self.send(author_id, parsing.bab(dayn, meal))
+
+            message = ''
